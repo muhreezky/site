20ace99c79854d16f4659ac20a69e4919b5ebd8b
Rizkiansyah <muhammadrizkiansyah60@gmail.com>
2024-10-26 23:20:07 +0700

    fix: mengubah isi env

--- src/lib/server/db/drizzle.ts
+++ src/lib/server/db/drizzle.ts
@@ -2,6 +2,7 @@ import { drizzle } from 'drizzle-orm/libsql';
 import 'dotenv/config';
 import { createClient } from '@libsql/client';
 import * as schema from './models/index';
+import { DB_TOKEN, DB_URL } from '$env/static/private';
 
-export const client = createClient({ url: process.env.DB_URL!, authToken: process.env.DB_TOKEN! });
+export const client = createClient({ url: DB_URL, authToken: DB_TOKEN });
 export const db = drizzle(client, { schema });
